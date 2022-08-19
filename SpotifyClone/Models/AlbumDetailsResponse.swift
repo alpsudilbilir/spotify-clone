10cdc23dc673dbe83e685c8a02b0d6163900966f
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-19 23:18:10 +0300

    Add and Update responses.

--- SpotifyClone/Models/AlbumDetailsResponse.swift
+++ SpotifyClone/Models/AlbumDetailsResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct AlbumDetailsResponse: Codable {
+    let album_type: String
+    let artists: [Artist]
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let label: String
+    let name: String
+    let tracks: TracksResponse
+    //    let available_markets: [String]
+
+}
+
+struct TracksResponse: Codable {
+    let items: [AudioTrack]
+}
