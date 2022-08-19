f51cdf8f4264c63fa627559b2e1823a6f3daa603
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-19 23:17:37 +0300

    Add playlist response model.

--- SpotifyClone/Models/PlaylistDetailsResponse.swift
+++ SpotifyClone/Models/PlaylistDetailsResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct PlaylistDetailsResponse: Codable {
+    let description: String
+    let external_urls: [String: String]
+    let id: String
+    let images: [APIImage]
+    let name: String
+    let tracks: PlaylistTracksResponse
+}
+
+struct PlaylistTracksResponse: Codable {
+    let items: [PlaylistItem]
+}
+
+struct PlaylistItem: Codable {
+    let track: AudioTrack
+}
