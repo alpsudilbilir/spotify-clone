ef7a29300e081f2d7cd82ee3a6021acad33bf095
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-20 01:10:18 +0300

    Create header for playlist view controller.

--- SpotifyClone/Views/PlaylistHeaderCollectionReusableView.swift
+++ SpotifyClone/Views/PlaylistHeaderCollectionReusableView.swift
@@ -6,7 +6,109 @@
 //
 
 import UIKit
+import SDWebImage
 
-class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
-        
+protocol PlaylistHeaderCollectionReusableViewDelegate: AnyObject {
+    func playlistHeaderCollectionReusableViewDidTapPlayAll(_ header: PlaylistHeaderCollectionReusableView)
+}
+final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "PlaylistHeaderCollectionReusableView"
+    
+    weak var delegate: PlaylistHeaderCollectionReusableViewDelegate?
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        return label
+    }()
+    private let ownerLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .thin)
+        label.numberOfLines = 0
+        return label
+    }()
+    private let playAllButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        button.setImage(UIImage(systemName: "play.fill"), for: .normal)
+        button.tintColor = .black
+        button.layer.cornerRadius = 25
+        button.layer.masksToBounds = true
+        return button
+    }()
+    
+    private let playlistImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage()
+        imageView.layer.cornerRadius = 8
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    //MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        addSubview(playlistImageView)
+        addSubview(ownerLabel)
+        addSubview(descriptionLabel)
+        addSubview(nameLabel)
+        addSubview(playAllButton)
+        playAllButton.addTarget(self, action: #selector(didTapPlayAll), for: .touchUpInside)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    @objc private func didTapPlayAll() {
+        delegate?.playlistHeaderCollectionReusableViewDidTapPlayAll(self)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let imageSize = width * 2/3
+        playlistImageView.frame = CGRect(
+            x: (width - imageSize) / 2,
+            y:  20,
+            width: imageSize,
+            height: imageSize)
+        nameLabel.frame = CGRect(
+            x: 10,
+            y: playlistImageView.bottom + 10 ,
+            width: width,
+            height: 24)
+        descriptionLabel.frame = CGRect(
+            x: 10,
+            y: nameLabel.bottom + 2,
+            width: width,
+            height: 24)
+        ownerLabel.frame = CGRect(
+            x: 10,
+            y: descriptionLabel.bottom + 2,
+            width: width,
+            height: 24)
+        playAllButton.frame = CGRect(
+            x: playlistImageView.rigth - 25,
+            y: playlistImageView.bottom - 25,
+            width: 50,
+            height: 50)
+    }
+    
+    func configure(with viewModel: PlaylistHeaderViewModel) {
+        nameLabel.text = viewModel.name
+        ownerLabel.text = viewModel.ownerName
+        descriptionLabel.text = viewModel.description
+        playlistImageView.sd_setImage(with: viewModel.artworkURL)
+    }
 }
