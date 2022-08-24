57a1e9f69c9895e4ff0dabb0ca0a708e2b6694c7
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-24 03:06:50 +0300

    Create search models.

--- SpotifyClone/Models/SearchResult.swift
+++ SpotifyClone/Models/SearchResult.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+enum SearchResult {
+    case artist(model: Artist)
+    case album(model: Album)
+    case track(model: AudioTrack)
+    case playlist(model: Playlist)
+}
