f17b978a591952b86a89e1a14e90b133191d52e2
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-27 12:54:57 +0300

    Update model.

--- SpotifyClone/Models/AudioTrack.swift
+++ SpotifyClone/Models/AudioTrack.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 struct AudioTrack: Codable {
-    let album: Album?
+    var album: Album?
     let artists: [Artist]
 //    let available_markets: [String]
     let disc_number: Int
@@ -15,5 +15,6 @@ struct AudioTrack: Codable {
     let explicit: Bool
     let external_urls: [String : String]
     let id: String
-    let name: String    
+    let name: String
+    let preview_url: String?
 }
