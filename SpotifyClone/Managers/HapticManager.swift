1334f13fadee7a5903d7077f93f2ca0e6e24f2cb
Alpsu Dilbilir <alpsudilbilir@gmail.com>
2022-08-30 16:08:54 +0300

    Add haptic manager.

--- SpotifyClone/Managers/HapticManager.swift
+++ SpotifyClone/Managers/HapticManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UIKit
+
+final class HapticsManager {
+    static let shared = HapticsManager()
+    
+    private init() { }
+    
+    
+    func vibrateForSelection() {
+        
+        DispatchQueue.main.async {
+            let generator = UISelectionFeedbackGenerator()
+            generator.prepare()
+            generator.selectionChanged()
+        }
+
+        
+    }
+    func vibrate(for type: UINotificationFeedbackGenerator.FeedbackType) {
+        DispatchQueue.main.async {
+            let generator = UINotificationFeedbackGenerator()
+            generator.prepare()
+            generator.notificationOccurred(type)
+        }
+
+        
+    }
+    
+    
+    
+}
